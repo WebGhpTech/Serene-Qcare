71024f354116adf52de1b733d0fe92c5daa3d48d
dom.wong <dom.wong@rpsgroup.com>
2023-11-09 21:19:22 +1000

    Adding recent files to main

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontSize: {
+      xs: ['12px', '16px'],
+      sm: ['14px', '20px'],
+      base: ['16px', '19.5px'],
+      lg: ['18px', '21.94px'],
+      xl: ['20px', '24.38px'],
+      '2xl': ['24px', '29.26px'],
+      '3xl': ['28px', '50px'],
+      '4xl': ['48px', '60px'],
+      '8xl': ['96px', '106px']
+    },
+    extend: {
+      fontFamily: {
+        palanquin: ['Palanquin', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'primary': "#7D62EB",
+        "secondary": "#1E293B",
+        "darkGray": "#475569",
+        "slate-gray-100": "#E2E8F0",
+        "blue": "#F0FDF4",
+        "button2":"#64748B",
+        "textGray":"#F1F5F9",
+        "purple":"#E5E0FB",
+      },
+      boxShadow: {
+        '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
+      },
+      backgroundImage: {
+        'hero': "url('assets/images/collection-background.svg')",
+        'card': "url('assets/images/thumbnail-background.svg')",
+      },
+      screens: {
+        "wide": "1440px"
+      }
+    },
+  },
+  plugins: [],
+}
