b04822c89cf21837919b66761c9dbe4b12abb0e7
WebGhpTech <149703588+WebGhpTech@users.noreply.github.com>
2023-11-17 00:31:37 +0800

    Update hover effect Nav.jsx

--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -40,7 +40,7 @@ const Nav = () => {
           ))}
         </ul>
 
-        <button className="text-white font-medium rounded py-3 px-4 bg-primary mr-10 hidden lg:block">Enquire Now</button>
+        <button className=" bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none text-white font-medium rounded py-3 px-4 bg-primary mr-10 hidden lg:block">Enquire Now</button>
       </nav>
     </header>
   );
