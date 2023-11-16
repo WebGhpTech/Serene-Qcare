16b9c4e204acbb659d45daaa0ddf3dbf4131c940
WebGhpTech <149703588+WebGhpTech@users.noreply.github.com>
2023-11-17 00:34:03 +0800

    Update hover effect Buttons.jsx

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -3,11 +3,11 @@
 const Buttons = () => {
   return (
     <div className="pl-7 flex gap-10 ease-in-button">
-    <button className="text-white bg-primary flex justify-center
+    <button className="bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none text-white bg-primary flex justify-center
      items-center gap-2 px-7 py-4 font-inter
      text-lg leading-none rounded
     ">Enquire Now </button>
-    <button className="text-white bg-button2 flex justify-center
+    <button className=" bg-gray-500 hover:bg-gray-600 active:bg-gray-700 focus:outline-none text-white bg-button2 flex justify-center
     items-center gap-2 px-7 py-4 font-inter
     text-lg leading-none rounded">Learn More </button>
     </div>
