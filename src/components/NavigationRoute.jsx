3b976a560444ea5d6cded04c5195b3e747de7857
WebGhpTech <149703588+WebGhpTech@users.noreply.github.com>
2023-11-20 15:26:39 +0800

    add pages route
    add pages route

--- src/components/NavigationRoute.jsx
+++ src/components/NavigationRoute.jsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
-import { Home, About } from '../pages';
+import { Home, About, Services, Team, Contact  } from '../pages';
 
 const NavigationRoute = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Home />} />
-      <Route path="/team" element={<Home />} />
-      <Route path="/contact" element={<Home />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/team" element={<Team />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   );
 };
