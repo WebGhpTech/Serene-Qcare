1d1a27d43497ce6965bb9bf3f3e679f75872692c
WebGhpTech <149703588+WebGhpTech@users.noreply.github.com>
2023-11-20 15:31:23 +0800

    add pages route
    add pages route

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
 import Home from './Home';
 import About from './About';
+import Services from './Services';
+import Team from './Team';
+import Contact from './Contact';
 
-export { Home, About };
+export { Home, About,Team, Services, Contact };
