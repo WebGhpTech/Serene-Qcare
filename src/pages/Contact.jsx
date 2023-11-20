2a6d492705c7980cb4bd6527c628c8a3117bbbbd
WebGhpTech <149703588+WebGhpTech@users.noreply.github.com>
2023-11-20 22:01:29 +0800

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,12 @@
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section className="bg-secondary h-[80vh]">
+         <div className="pt-[150px] w-full pb-0 text-white ml-[100px] justify-center" >
+            <div>
+               This is Contact
+            </div>
+         </div>
+    </section>
   )
 }
 
