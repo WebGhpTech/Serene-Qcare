207bf32005a5bd3970b62ccf29a4c14fb262ff7a
WebGhpTech <149703588+WebGhpTech@users.noreply.github.com>
2023-11-20 22:01:05 +0800

    Update Team.jsx

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,6 +1,12 @@
 const Team = () => {
   return (
-    <div>Team</div>
+    <section className="bg-secondary h-[80vh]">
+         <div className="pt-[150px] w-full pb-0 text-white ml-[100px] justify-center" >
+            <div>
+               This is Team Page
+            </div>
+         </div>
+    </section>
   )
 }
 
