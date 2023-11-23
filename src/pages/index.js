76103b8c42c756d276470e8a16b524dc5ecb89ce
WebGhpTech <149703588+WebGhpTech@users.noreply.github.com>
2023-11-23 13:16:35 +0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import About from './About';
 import Services from './Services';
 import Team from './Team';
 import Contact from './Contact';
-import Receive from './Receive'
 
 
-export { Home, About, Team, Services, Contact, Receive};
+
+export { Home, About, Team, Services, Contact, };
