1533d01bfdc8f629ee74b6a8680c423263623a40
WebGhpTech <149703588+WebGhpTech@users.noreply.github.com>
2023-11-23 12:55:33 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -31,6 +31,11 @@ const routes = [
     title: 'Contact Us',
     component: Contact, //replace with Contact component when available
   },
+   {
+    path: '/contact',
+    exact: true,
+  component: Receive,
+  },
   
 ];
 
